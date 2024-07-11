8a66859f66fdff4237037923f5ac00fafef92880
Marvin Mbutura <marvinmbutura@Marvins-MacBook-Pro.local>
2024-07-11 12:04:59 +0300

    Second commit

---
+++ grade.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')();
+
+let marks = Number(prompt("Enter Student's Marks: "));
+
+if (marks<=100 && marks>=79) 
+{
+    console.log("A")
+} else if(marks<79 && marks>=60) {
+    console.log ("B")
+} else if(marks<60 && marks>=49) {
+    console.log("C")
+} else if(marks<49 && marks>=40) {
+    console.log ("D")
+} else if(marks<40) {
+    console.log ("E")
+} else if(isNaN(marks)) {
+// value is not a number
+console.log("error!");
+}
